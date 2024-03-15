7db0ba64e442dbfbe1041d6a1deeae0b9133091c
aravind hariharaputran <araharip@amazon.com>
2024-03-14 22:10:41 -0400

    rds2014-upgradev2

--- README.md
+++ README.md
@@ -11,9 +11,24 @@ This process is a single-click deployment that will enable customers perform the
 - AWS CLI latest version
 - Python latest version
 - Source SQL RDS Instance to upgrade
-- `Note` : Tool will   run successfully only when compatabile and supported values are passed. For more information, refer to  :https://docs.aws.amazon.com/AmazonRDS/latest/UserGuide/CHAP_SQLServer.html for more information
-Example if 2014 SQL Server RDS has SSAS DB Option enabled, when you try to copy DB Parameters to SQL Server 2022, this will fail since SSAS is not supported by RDS for SQL Server.
-If you have SSRS DB option enabled on the source RDS,provide permission on the SSRS Secrets in Secrets manager. For more information, refer to https://docs.aws.amazon.com/AmazonRDS/latest/UserGuide/Appendix.SQLServer.Options.SSRS.html
+
+
+> **⚠️ Note**
+>
+>Tool will successfully run  only when compatabile and supported values are passed. For more information , refer to  :https://docs.aws.amazon.com/AmazonRDS/latest/UserGuide/CHAP_SQLServer.html for more information 
+>
+>If you are trying to enable MultiAZ on RDS for SQL Server 2014 that has MSDTC Option enabled, this will fail since MultiAZ is not supported when MSDTC is enabled.
+>
+>If you have SSAS enabled on RDS for SQL Server 2014 , if you try to upgrade to on RDS for SQL Server 2022, this will fail as SSAS is not supported in on RDS for SQL Server 2022.
+>
+>If you have SSRS DB option enabled on the source RDS,make sure to provide permission on the SSRS Secrets's resource policy to the new Option Group name, that you will pass during the script execution (example : SQL-RDS-2022-OG) in Secrets manager. Only then SSRS copy will be successful. For more information, refer to https://docs.aws.amazon.com/AmazonRDS/latest/UserGuide/Appendix.SQLServer.Options.SSRS.html>
+>
+>If you are running on SQL Server 2014 with MultiAZ  and trying to upgrade to SQL Server 2022 this will fail . As per upgrade path you need to either convert Multi AZ to SingleAZ first or upgrade to intermediate verison before upgrading to 2022. MultiAZ to SingleAZ conversion is not handled by this tool. Once you manually convert them you can rerun this tool.
+>
+>Make sure to evaluate and remove any paramters that are not needed in the upgraded version . This tool will copy all the parameters from source to target version.
+>
+
+
 
 ## How to Run this ?
 
--- rds_upgrade_2014.sh
+++ rds_upgrade_2014.sh
@@ -1,11 +1,10 @@
-
 #Author Aravind Kumar Hariharaputran
 #Version 1
 #Date : 03/04/2024
 
 ########################################################################################## High level Steps##############################################################################################
 
-#Convert singleAz into multi-Az
+#Convert singleAZ into multi-Az 
 #Create new parameter group for target version
 #Copy the parameters to target parameter group 
 #Create new option group for target version
@@ -16,36 +15,32 @@
 #Reboot the RDS instance
 
 
-
-#Prerequisites
-#AWS CLI latest version
-#Python latest version
-#Source SQL RDS Instance to upgrade
-#Note : Tool will   run successfully only when compatabile and supported values are passed. For more information, refer to  :https://docs.aws.amazon.com/AmazonRDS/latest/UserGuide/CHAP_SQLServer.html
-#Example if your RDS for SQL Server 2014 has SSAS DB Option enabled, when you try to copy DB Parameters to  RDS for SQL Server 2022, this will fail since SSAS is not supported by RDS for SQL Server 2022.
-#If you are trying to enable MultiAZ on RDS for SQL Server 2014 that has MSDTC Option enabled, this will fail since MultiAZ is not supported when MSDTC is enabled.
-#If you have SSRS DB option enabled on the source RDS,provide permission on the SSRS Secrets to the new Option Group (target_option_group_name) in Secrets manager. Only then SSRS copy will be successful. For more information, refer to https://docs.aws.amazon.com/AmazonRDS/latest/UserGuide/Appendix.SQLServer.Options.SSRS.html
-
-
 #############################################################################################################################################################################################################
 
 
 rm -f upgrade_output.log
-date '+%Y-%m-%d %H:%M:%S'>>upgrade_output.log
+echo -e "Welcome to RDSUpgrade Tool\n">>upgrade_output.log
+date '+%Y-%m-%d %H:%M:%S' >>upgrade_output.log
 start=`date +%s`
 
-source_db_instance_identifier='rds-upgrade-testing'
+
+source_db_instance_identifier='rds2016saz'
 region='us-east-2'
 source_db_parameter_group_name=''
 source_engineversion=''
 source_db_parameter_group_family=''
-
+source_engineversion=''
 target_db_parameter_group_name='test-upgrade-pg16'
 target_db_parameter_group_family='sqlserver-se-16.0'
 target_option_group_name='test-upgrade-og16'
 target_engine_name='sqlserver-se'
 target_major_engine_version='16.00'
 target_engine_version='16.00.4095.4.v1'
+low_mzvalue_true="true"
+low_mzvalue_false="false"
+
+
+
 
 echo ----------------------------------------------------------------------------------------------------------------------------------------------------
 echo ----------------------------------------------------------------------------------------------------------------------------------------------------
@@ -55,88 +50,72 @@ echo ---------------------------------------------------------------------------
 echo ----------------------------------------------------------------------------------------------------------------------------------------------------
 echo ----------------------------------------------------------------------------------------------------------------------------------------------------
 
+echo "AWS SQL RDS INPLACE-UPGRADE" | sed  -e :a -e "s/^.\{1,$(tput cols)\}$/ & /;ta" | tr -d '\n' >>upgrade_output.log 
+
+
 echo "Enter Source RDS Instance identifier"
 read source_db_instance_identifier
+echo -e "\nRDS Instance identifier $source_db_instance_identifier">>upgrade_output.log 
+
 
 echo "Enter Source RDS Instance AWS Region"
 read region
+echo -e "RDS Instance AWS Region $region">>upgrade_output.log
 
 echo "Enter RDS Instance target parameter group name"
 read target_db_parameter_group_name
+echo -e "RDS Instance target parameter group name $target_db_parameter_group_name">>upgrade_output.log
 
 echo "Enter RDS Instance target parameter group family(Example:sqlserver-se-15.0)"
 read target_db_parameter_group_family
+echo -e "RDS Instance target parameter group family $target_db_parameter_group_family">>upgrade_output.log
 
 echo "Enter RDS Instance target option group name"
 read target_option_group_name
-
-
-echo "Enter RDS Instance target engine name(Example:sqlserver-se)"
-read target_engine_name
-
-echo "Enter RDS Instance target major engine version(Example:15.00)"
-read target_major_engine_version
+echo -e "RDS Instance target option group name $target_option_group_name">>upgrade_output.log
 
 echo "Enter RDS Instance target engine version(Example:15.00.4345.5.v1)"
 read target_engine_version
+echo -e "RDS Instance target engine version $target_engine_version">>upgrade_output.log
+
+sql2014=12
+sql2016=13
+sql2017=14
+sql2019=15
+sql2022=16
+source_engineversion_temp=`aws rds describe-db-instances --db-instance-identifier $source_db_instance_identifier --query 'DBInstances[*].EngineVersion'|sed 's/[^0-9]//g'`
+source_engineversion=${source_engineversion_temp:0:3}
+target_db_parameter_group_family_temp=$target_db_parameter_group_family
+target_engine_name=${target_db_parameter_group_family_temp:0:12}
+target_major_engine_version="${target_db_parameter_group_family_temp:13:17}0"
+target_major_engine_version_maz_temp=$target_major_engine_version
+target_major_engine_version_maz=${target_major_engine_version_maz_temp:0:2}
+echo -e "\nSource Engine Major version: $source_engineversion">>upgrade_output.log
+echo -e "\nTareget Engine Major Version MAZ:$target_major_engine_version_maz">>upgrade_output.log
+echo -e "\nTarget Engine Name: $target_engine_name">>upgrade_output.log
+
+multiaz=`aws rds describe-db-instances --db-instance-identifier $source_db_instance_identifier --query 'DBInstances[*].MultiAZ|[0]'`
+echo "RDS MultiAZ: $multiaz">>upgrade_output.log
+lis_endpoint=`aws rds describe-db-instances --db-instance-identifier $source_db_instance_identifier --query 'DBInstances[*].ListenerEndpoint.Address|[0]'|sed -r 's/"//g'`
+echo "Listener  endpoint: $lis_endpoint">>upgrade_output.log
+echo "Tareget Major Version $target_major_engine_version">>upgrade_output.log
 
 
-echo "RDS instance's SQL Server Engine before upgrade">>upgrade_output.log 
-source_engineversion=`aws rds describe-db-instances --db-instance-identifier $source_db_instance_identifier --query 'DBInstances[*].EngineVersion'`
-source_engineversion=`sed -e 's/^"//' -e 's/"$//' <<<"$source_engineversion"`
-echo $source_engineversion>>upgrade_output.log 
+#low_mzvalue_true="false"
+echo "$low_mzvalue_true"
+target_major_engine_version_maz=15
+echo "11 $target_major_engine_version_maz"
 
+if [[ ($lis_endpoint =~ (null)) && ($multiaz =~ ($low_mzvalue_true)) && ($target_major_engine_version_maz -eq 16) ]]; then   
+    echo "Target Major Engine version  is $target_major_engine_version ,SQL Server 2022. As per the upgrade path to SQL Server 2022, MultiAZ should be converted to SingleAZ first.Please convert that in the console and rerun this tool"
+    break
+    else
+    echo "Configuration looks good for the upgrade path. Continuing the process....!"
+fi
 
-echo "If your RDS is SingleAz ,would you like to convert singleAz to multiAz? If so,please ensure to have automatic backup enabled on RDS to proceed (y/n)?"
-read answer
-case $answer in 
-        [yY] ) 
-        multiaz=`aws rds describe-db-instances --db-instance-identifier $source_db_instance_identifier --query 'DBInstances[*].MultiAZ|[0]'`
-        echo "Value of MultiAZ is $multiaz">>upgrade_output.log      
-            if [ $multiaz == "false" ]; then
-                    date '+%Y-%m-%d %H:%M:%S'>>upgrade_output.log
-                    echo "Converting Single AZ to MultiAZ">>upgrade_output.log  
-                    while true
-                    do  
-                        sleep 30
-                        dbstatus=$(aws rds describe-db-instances --db-instance-identifier $source_db_instance_identifier --query 'DBInstances[*].DBInstanceStatus|[0]')
-                        dbstatus=`sed -e 's/^"//' -e 's/"$//' <<<"$dbstatus"`
-                        if [ ${dbstatus} == "available" ];then
-                            break;
-                        else
-                            echo "Pease wait DB is not in available state">>upgrade_output.log
-                            date '+%Y-%m-%d %H:%M:%S'>>upgrade_output.log
-                            sleep 30
-                        fi
-                    done
-                    aws rds modify-db-instance --db-instance-identifier $source_db_instance_identifier --multi-az --apply-immediately>/dev/null
-                    while true
-                    do  
-                        sleep 30
-                        dbstatus=$(aws rds describe-db-instances --db-instance-identifier $source_db_instance_identifier --query 'DBInstances[*].DBInstanceStatus|[0]')
-                        dbstatus=`sed -e 's/^"//' -e 's/"$//' <<<"$dbstatus"`
-                        if [ ${dbstatus} == "available" ];then
-                            break;
-                        else
-                            echo "MultiAZ conversion is in progress . Please wait">>upgrade_output.log
-                            date '+%Y-%m-%d %H:%M:%S'>>upgrade_output.log
-                            sleep 30
-                        fi
-                    done
-
-                else                 
-                    echo "RDS is already multiAZ">>upgrade_output.log
-            fi
-            ;;
-        [nN] ) 
-        echo "Continuing the upgrade without AZ conversion">>upgrade_output.log
-            ;;
-
-        * ) echo Invalid response. Please try again;;
-esac
-
-
-
+##############################################################################################################################################################################################################################################
+##Parameter and Option Group handling
+##############################################################################################################################################################################################################################################
 
 echo Getting the source parameter group name from RDS instance>>upgrade_output.log 
 source_db_parameter_group_name=`aws rds describe-db-instances --db-instance-identifier $source_db_instance_identifier --region $region --query 'DBInstances[*].DBParameterGroups[].DBParameterGroupName|[0]'`
@@ -157,7 +136,7 @@ if [[ $source_db_parameter_group_name =~ $default_pg ]]; then
     else 
     echo "Custom ParameterGroup $source_db_parameter_group_name found on the source.">>upgrade_output.log
     echo "Creating new parameter group $target_db_parameter_group_name for target version">>upgrade_output.log
-    aws rds create-db-parameter-group  --db-parameter-group-name $target_db_parameter_group_name  --region $region --db-parameter-group-family $target_db_parameter_group_family --description 'SQL RDS Upgrade ParameterGroup'>/dev/null
+    aws rds create-db-parameter-group  --db-parameter-group-name $target_db_parameter_group_name  --region $region --db-parameter-group-family $target_db_parameter_group_family --description 'SQL RDS Upgrade ParameterGroup'>>upgrade_output.log
     date '+%Y-%m-%d %H:%M:%S'>>upgrade_output.log
     echo "Selecting only modified parameters from  the source">>upgrade_output.log
     aws rds describe-db-parameters --db-parameter-group-name $source_db_parameter_group_name --region $region  --query "Parameters[?Source=='user']" --output json>source_pg.json
@@ -173,21 +152,25 @@ if [[ $source_option_group_name =~ $default_og ]]; then
     else 
     echo 'Custom OptionGroup $source_db_option_group_name found on the source.'>>upgrade_output.log
     echo "Creating new Option group $target_option_group_name for target version">>upgrade_output.log
-    aws rds create-option-group  --option-group-name $target_option_group_name  --region $region  --engine-name $target_engine_name --major-engine-version $target_major_engine_version --option-group-description 'SQL RDS Upgrade optionGroup'>/dev/null
+    aws rds create-option-group  --option-group-name $target_option_group_name  --region $region  --engine-name $target_engine_name --major-engine-version $target_major_engine_version --option-group-description 'SQL RDS Upgrade optionGroup'>>upgrade_output.log
     date '+%Y-%m-%d %H:%M:%S'>>upgrade_output.log  
     echo "Copying the options from the source option group to the target">>upgrade_output.log
-    aws rds   describe-option-groups --option-group-name $source_db_option_group_name --region $region  --query "OptionGroupsList[*].Options">og_input.json
+    aws rds describe-option-groups --option-group-name $source_db_option_group_name --region $region  --query "OptionGroupsList[*].Options">og_input.json
     py db_options.py
-    aws rds  add-option-to-option-group --option-group-name $target_option_group_name --options file://og_output.json>/dev/null
+    aws rds add-option-to-option-group --option-group-name $target_option_group_name --options file://og_output.json>>upgrade_output.log
     date '+%Y-%m-%d %H:%M:%S'>>upgrade_output.log
 
 fi
+##############################################################################################################################################################################################################################################
+##InPlace Upgrade 
+##############################################################################################################################################################################################################################################
+
 
 echo "Performing In Place upgrade now">>upgrade_output.log
 date '+%Y-%m-%d %H:%M:%S'>>upgrade_output.log
 while true
  do  
-sleep 30
+sleep 01
 dbstatus=$(aws rds describe-db-instances --db-instance-identifier $source_db_instance_identifier --query 'DBInstances[*].DBInstanceStatus|[0]')
 dbstatus=`sed -e 's/^"//' -e 's/"$//' <<<"$dbstatus"`
     if [ ${dbstatus} == "available" ];then
@@ -195,13 +178,13 @@ dbstatus=`sed -e 's/^"//' -e 's/"$//' <<<"$dbstatus"`
         else
             echo "Pease wait DB is not in available state">>upgrade_output.log
             date '+%Y-%m-%d %H:%M:%S'>>upgrade_output.log
-            sleep 30
+            sleep 01
     fi
 done
-aws rds   modify-db-instance --db-instance-identifier $source_db_instance_identifier --engine-version $target_engine_version --db-parameter-group-name $target_db_parameter_group_name  --option-group-name $target_option_group_name --allow-major-version-upgrade --apply-immediately>/dev/null
+aws rds   modify-db-instance --db-instance-identifier $source_db_instance_identifier --engine-version $target_engine_version --db-parameter-group-name $target_db_parameter_group_name  --option-group-name $target_option_group_name --allow-major-version-upgrade --apply-immediately>>upgrade_output.log
 while true;
 do
-    sleep 30
+    sleep 01
     dbstatus=$(aws rds describe-db-instances --db-instance-identifier $source_db_instance_identifier --query 'DBInstances[*].DBInstanceStatus|[0]')
     dbstatus=`sed -e 's/^"//' -e 's/"$//' <<<"$dbstatus"`
     if [ ${dbstatus} == "available" ];then
@@ -209,14 +192,14 @@ do
     else
         echo "Upgrade is in progress . Please wait">>upgrade_output.log
         date '+%Y-%m-%d %H:%M:%S'>>upgrade_output.log
-        sleep 30
+        sleep 01
     fi
 done
 echo "Rebooting RDS instance">>upgrade_output.log
-aws rds reboot-db-instance --db-instance-identifier $source_db_instance_identifier --region $region>/dev/null
+aws rds reboot-db-instance --db-instance-identifier $source_db_instance_identifier --region $region>>/dev/null
 while true;
 do
-     sleep 30
+     sleep 01
     dbstatus=$(aws rds describe-db-instances --db-instance-identifier $source_db_instance_identifier --query 'DBInstances[*].DBInstanceStatus|[0]')
     dbstatus=`sed -e 's/^"//' -e 's/"$//' <<<"$dbstatus"`
     if [ ${dbstatus} == "available" ];then
@@ -224,7 +207,7 @@ do
     else
         echo "Reboot is in progress . Please wait">>upgrade_output.log
         date '+%Y-%m-%d %H:%M:%S'>>upgrade_output.log
-        sleep 30
+        sleep 01
     fi
 done
 
@@ -232,9 +215,3 @@ echo "RDS instance's SQL Server Engine after upgrade">>upgrade_output.log
 source_engineversion=`aws rds describe-db-instances --db-instance-identifier $source_db_instance_identifier --query 'DBInstances[*].EngineVersion'`
 source_engineversion=`sed -e 's/^"//' -e 's/"$//' <<<"$source_engineversion"`
 echo $source_engineversion>>upgrade_output.log 
-
-echo "Upgrade completed successfully">>upgrade_output.log
-end=`date +%s`
-expr=$((end-start))
-echo 'Execution time in HH MM SS' >>upgrade_output.log
-echo $((expr/3600)) $((expr%3600/60)) $((expr%60))>>upgrade_output.log
